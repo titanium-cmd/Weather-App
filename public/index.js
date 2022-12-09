db70797d1b21cabd47b9ade245bbee89e05cc461
titanium-cmd <nkansahisaac41@gmail.com>
2022-12-09 18:49:33 +0000

    added api key to project

--- public/index.js
+++ public/index.js
@@ -1,11 +1,9 @@
-require('dotenv').config();
-
-window.addEventListener('load', () => {
+window.addEventListener('load', ()=>{
     const getLocBtn = document.querySelector('#top-section > button');
     const searchBtn = document.querySelector('#search-bar > button');
     const searchBox = document.querySelector('#search-bar > input');
     const BASE_URL = `https://api.openweathermap.org/data/2.5/weather`;
-    const API_KEY = process.env.API_KEY;
+    const API_KEY = 'eb0a59e5a9220a6b8cd53e09cbdfd98a';
     getCurrentWeather(BASE_URL, API_KEY);
 
     getLocBtn.addEventListener("click", ()=>{
